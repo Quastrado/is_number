997d1e7059b1110093c44cf73e1079db8ae3725d
Quastrado <quastrado@gmail.com>
2021-01-26 15:12:37 +0300

    fix imports in test

--- tests/test_is_number.py
+++ tests/test_is_number.py
@@ -1,10 +1,10 @@
 import unittest
-from is_number.__init__ import is_number
+from is_number.is_number import is_number
 
 
 class JustTest(unittest.TestCase):
 
 
-    def test_rright_assert(self):
+    def test_right_assert(self):
         result = is_number(20)
         self.assertEqual(result, True)
